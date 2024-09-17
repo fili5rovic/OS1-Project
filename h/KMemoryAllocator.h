b7fb4168c11490042863f8bc7ae1a98de8b2c391
fili5rovic <fili5rovic@gmail.com>
2024-09-17 11:42:09 -0700

    Completed linked list structure for alloc()

--- h/KMemoryAllocator.h
+++ h/KMemoryAllocator.h
@@ -9,14 +9,9 @@
 #include "../lib/console.h"
 
 struct Node {
-    Node *next;
+    Node* next;
+    Node* prev;
     uint64 size;
-    uint64 addr;
-
-    Node(const uint64 s) {
-        this->size = s;
-        this->next = nullptr;
-    }
 };
 
 class KMemoryAllocator {
@@ -26,47 +21,82 @@ public:
         return instance;
     }
 
-    void *allocate(const size_t size) {
-        size_t newSize = size;
-        if (size % MEM_BLOCK_SIZE != 0) {
-            newSize -= size % MEM_BLOCK_SIZE;
+    KMemoryAllocator(KMemoryAllocator const &) = delete;
+    KMemoryAllocator(KMemoryAllocator &&) = delete;
+    KMemoryAllocator &operator=(KMemoryAllocator const &) = delete;
+    KMemoryAllocator &operator=(KMemoryAllocator &&) = delete;
+
+
+    void* allocate(const size_t _size) {
+        size_t newSize = _size + sizeof(Node);
+        if (newSize % MEM_BLOCK_SIZE != 0) {
+            newSize -= newSize % MEM_BLOCK_SIZE;
             newSize += MEM_BLOCK_SIZE;
         }
-
+        nextAddress = 0;
         Node* current = freeHead;
-        while(current != nullptr) {
-            if(current->size >= newSize) {
-                nextAddress = current->addr;
-                // sad treba da ga pomerim za size
-                if(current->addr + newSize < current->size) {
-                    current->addr += newSize;
-                } else {
-                    // todo delete current
+        while (current != nullptr) {
+            // first fit
+            if (current->size >= newSize) {
+                // if current has more space, make another free node from the rest
+                const bool overLimit = reinterpret_cast<uint64>(current) + newSize + sizeof(Node) >= reinterpret_cast<
+                                           uint64>(HEAP_END_ADDR);
+                Node* nNode = nullptr;
+                if (current->size - newSize >= MEM_BLOCK_SIZE && !overLimit) {
+                    nNode = (current + newSize);
+                    nNode->prev = current->prev;
+                    nNode->next = current->next;
+                    nNode->size = current->size - newSize;
                 }
-                // ----
+
+                if (current->prev != nullptr)
+                    current->prev->next = nNode ? nNode : current->next;
+                if (current->next != nullptr)
+                    current->next->prev = nNode ? nNode : current->prev;
+
+                current->next = nullptr;
+                current->prev = nullptr;
+                current->size = newSize;
+                nextAddress = reinterpret_cast<uint64>(current) + sizeof(Node);
                 break;
             }
             current = current->next;
         }
+        if(nextAddress == 0)
+            return nullptr;
 
-        void* allocatedMemory = reinterpret_cast<void *>(nextAddress);
-        nextAddress += newSize;
+
+        void* allocatedMemory = reinterpret_cast<void*>(nextAddress);
 
         return allocatedMemory;
     }
+
+
 
 private:
     KMemoryAllocator() {
         nextAddress = reinterpret_cast<uint64>(HEAP_START_ADDR);
 
-        freeHead = (Node*)((char*)HEAP_START_ADDR);
-        freeHead->next = nullptr;
-        freeHead->addr = nextAddress;
-        freeHead->size = reinterpret_cast<uint64>(HEAP_END_ADDR) - nextAddress - sizeof(Node);
+        freeHead = (Node*) ((char*) nextAddress);
         nextAddress += sizeof(Node);
+        freeHead->next = nullptr;
+        freeHead->prev = nullptr;
+        freeHead->size = reinterpret_cast<uint64>(HEAP_END_ADDR) - nextAddress - sizeof(Node);
     }
 
-    Node *freeHead;
+
+    void addFreeNode(Node* node) {
+        if (node == nullptr)
+            return;
+        Node* current = freeHead;
+        while (current->next != nullptr) {
+            current = current->next;
+        }
+        current->next = node;
+        node->prev = current;
+    }
+
+    Node* freeHead;
     uint64 nextAddress;
 };
 
